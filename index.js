d82a6ba7432cae8dc7c116f1ad59b400bbd40600
Zeref26 <killianpoulain26@gmail.com>
2019-03-13 16:47:24 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -38,13 +38,6 @@ bot.on('message', message => {
                                 } else {
                                     message.channel.send(adv.displayName+" évite le tir de "+lanc.displayName);
                                 }
-                            } else if (tir>=90) {
-                                let esq = Math.floor(Math.random() * (100)+1);
-                                if (esq < 80) {
-                                    message.channel.send(lanc.displayName+" réussit son tir rebondissant.");
-                                } else {
-                                    message.channel.send(adv.displayName+" évite le tir rebondissant de "+lanc.displayName+" !");
-                                }
                             } else {
                                 message.channel.send(lanc.displayName+" a raté son tir.");
                             }
@@ -63,13 +56,6 @@ bot.on('message', message => {
                             message.channel.send(lanc.displayName+" réussit son tir.");
                         } else {
                             message.channel.send(adv.displayName+" évite le tir de "+lanc.displayName);
-                        }
-                    } else if (tir>=90) {
-                        let esq = Math.floor(Math.random() * (100)+1);
-                        if (esq < 80) {
-                            message.channel.send(lanc.displayName+" réussit son tir rebondissant.");
-                        } else {
-                            message.channel.send(adv.displayName+" évite le tir rebondissant de "+lanc.displayName+" !");
                         }
                     } else {
                         message.channel.send(lanc.displayName+" a raté son tir.");
@@ -105,6 +91,66 @@ bot.on('message', message => {
             } else {
                 message.channel.send("Vous devez indiquer une personne.");
             }
+        } else {
+            message.channel.send("Vous devez indiquer une personne.");
+        }
+    }
+    if (message.content.startsWith('>shoot_reb') && member.roles.exists('name', "Exceed")) {
+        const args = message.content.slice(1).trim().split(/ +/g);
+        message.delete();
+        let lanc = message.guild.members.find('id', message.author.id);
+        if (message.mentions.members.size>=1 && args.length>1) {
+            let trouve = 0;
+            let adv = message.mentions.members.first();
+            bot.channels.get("555145541659525123").fetchMessages({limit:99}).then(messages => {
+                messages.forEach((msg) => {
+                    let lp = "";
+                    if (msg.content.includes(lanc.displayName)) {
+                        trouve = 1;
+                        let m = ""+msg.content;
+                        let l = m.length;
+                        for (let i = 0; i<l; i++) {
+                            if (m.charAt(i)==':') {
+                                for (let j = i+2; j<l; j++) {
+                                    lp += m.charAt(j);
+                                }                                
+                            }
+                        }
+                        if (parseInt(lp)>0) {
+                            let tir = Math.floor(Math.random() * (100)+1);
+                            if (tir>=90) {
+                                let esq = Math.floor(Math.random() * (100)+1);
+                                if (esq < 80) {
+                                    message.channel.send(lanc.displayName+" réussit son tir rebondissant.");
+                                } else {
+                                    message.channel.send(adv.displayName+" évite le tir rebondissant de "+lanc.displayName+" !");
+                                }
+                            } else {
+                                message.channel.send(lanc.displayName+" a raté son tir.");
+                            }
+                            new_m = lanc.displayName+" : "+(parseInt(lp)-1);
+                            msg.edit(new_m);
+                        } else {
+                            message.channel.send("Vous n'avez pas assez de LP.");
+                        }
+                    }
+                });
+                if (trouve == 0) {
+                    let tir = Math.floor(Math.random() * (100)+1);
+                    if (tir>=90) {
+                        let esq = Math.floor(Math.random() * (100)+1);
+                        if (esq < 80) {
+                            message.channel.send(lanc.displayName+" réussit son tir rebondissant.");
+                        } else {
+                            message.channel.send(adv.displayName+" évite le tir rebondissant de "+lanc.displayName+" !");
+                        }
+                    } else {
+                        message.channel.send(lanc.displayName+" a raté son tir.");
+                    }
+                    new_m = lanc.displayName+" : 9";
+                    bot.channels.find('id',"555145541659525123").send(new_m);
+                }
+            });
         } else {
             message.channel.send("Vous devez indiquer une personne.");
         }
