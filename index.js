e9fcf8a16247ab07afe4c67aa8828386511f3d9c
Zeref26 <killianpoulain26@gmail.com>
2019-03-23 12:24:04 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ let bot = new Discord.Client();
 
 bot.on('ready', () => {
     console.log('Bot is ready !');
-    bot.user.setActivity("« Dess »");
 });
 
 bot.on('message', message => {
+
     let serv = bot.guilds.find('id', "505007497770434600");
     let member = serv.members.find('id', message.author.id);
+
     if (message.content.startsWith('>shoot_n') && member.roles.exists('name', "Exceed")) {
         const args = message.content.slice(1).trim().split(/ +/g);
         message.delete();
@@ -35,7 +36,15 @@ bot.on('message', message => {
                             if (tir>=50) {
                                 let esq = Math.floor(Math.random() * (100)+1);
                                 if (esq < 50) {
-                                    message.channel.send(lanc.displayName+" réussit son tir.");
+                                    let num = Math.floor(Math.random() * 3 + 1);
+                                    switch (num) {
+                                        case 1 : message.channel.send(lanc.displayName+" réussit son tir sur "+adv.displayName+".");
+                                            break;
+                                        case 2 : message.channel.send(lanc.displayName+" exécute un magnifique tir sur "+adv.displayName+".");
+                                            break;
+                                        case 3 : message.channel.send("Le tir de "+lanc.displayName+" semble avoir touché "+adv.displayName+".");
+                                            break;
+                                    }
                                 } else {
                                     message.channel.send(adv.displayName+" évite le tir de "+lanc.displayName);
                                 }
@@ -54,7 +63,15 @@ bot.on('message', message => {
                     if (tir>=50) {
                         let esq = Math.floor(Math.random() * (100)+1);
                         if (esq < 50) {
-                            message.channel.send(lanc.displayName+" réussit son tir.");
+                            let num = Math.floor(Math.random() * 3 + 1);
+                            switch (num) {
+                                case 1 : message.channel.send(lanc.displayName+" réussit son tir sur "+adv.displayName+".");
+                                    break;
+                                case 2 : message.channel.send(lanc.displayName+" exécute un magnifique tir sur "+adv.displayName+".");
+                                    break;
+                                case 3 : message.channel.send("Le tir de "+lanc.displayName+" semble avoir touché "+adv.displayName+".");
+                                    break;
+                            }
                         } else {
                             message.channel.send(adv.displayName+" évite le tir de "+lanc.displayName);
                         }
@@ -122,7 +139,15 @@ bot.on('message', message => {
                             if (tir>=66) {
                                 let esq = Math.floor(Math.random() * (100)+1);
                                 if (esq < 75) {
-                                    message.channel.send(lanc.displayName+" réussit son tir rebondissant.");
+                                    let num = Math.floor(Math.random() * 3 + 1);
+                                    switch (num) {
+                                        case 1 : message.channel.send(lanc.displayName+" réussit son tir rebondissant sur "+adv.displayName+".");
+                                            break;
+                                        case 2 : message.channel.send(lanc.displayName+" exécute un magnifique tir rebondissant sur "+adv.displayName+".");
+                                            break;
+                                        case 3 : message.channel.send("Le tir rebondissant de "+lanc.displayName+" semble avoir touché "+adv.displayName+".");
+                                            break;
+                                    }
                                 } else {
                                     message.channel.send(adv.displayName+" évite le tir rebondissant de "+lanc.displayName+" !");
                                 }
@@ -141,7 +166,15 @@ bot.on('message', message => {
                     if (tir>=66) {
                         let esq = Math.floor(Math.random() * (100)+1);
                         if (esq < 75) {
-                            message.channel.send(lanc.displayName+" réussit son tir rebondissant.");
+                            let num = Math.floor(Math.random() * 3 + 1);
+                            switch (num) {
+                                case 1 : message.channel.send(lanc.displayName+" réussit son tir rebondissant sur "+adv.displayName+".");
+                                    break;
+                                case 2 : message.channel.send(lanc.displayName+" exécute un magnifique tir rebondissant sur "+adv.displayName+".");
+                                    break;
+                                case 3 : message.channel.send("Le tir rebondissant de "+lanc.displayName+" semble avoir touché "+adv.displayName+".");
+                                    break;
+                            }
                         } else {
                             message.channel.send(adv.displayName+" évite le tir rebondissant de "+lanc.displayName+" !");
                         }
@@ -227,6 +260,61 @@ bot.on('message', message => {
         let lanc = message.guild.members.find('id', message.author.id);
         message.channel.send("La liste des commandes vous a été envoyée en privé.");
         lanc.send("Voici la liste des commandes : \n\n **- >shoot_n @membre** Execute une tentative de tir sur @membre. Consomme 1 LP. \n **- >shoot_r @membre** Execute une tentative de tir rebondissant sur @membre. Consomme 1 LP. \n **- >shoot_IA** Vous tirez sur une IA. Vous regagnez 2 LP. \n **- >illusion @membre** Effectue une tentative pour faire rentrer @membre dans votre illusion. Uniquement les dhampires. \n **- >end** Finit votre partie.");
+    }
+    if (message.content.startsWith('>dess')) {
+        let num = Math.floor(Math.random()*25 + 1);
+        switch (num) {
+            case 1 : message.channel.send("Tu fais quoi là à me caresser, dess ? Mais continue crétin, dess.");
+                break;
+            case 2 : message.channel.send("Espèce de singe imberbe, caresse moi, dess.");
+                break;
+            case 3 : message.channel.send("J’ai faim, donne moi encore à manger, dess.");
+                break;
+            case 4 : message.channel.send("Je suis fatiguée. Laisse moi dormir, dess.");
+                break;
+            case 5 : message.channel.send("Pourquoi es tu aussi idiot, dess ?");
+                break;
+            case 6 : message.channel.send("Mon grand père peut t’arracher la moelle épinière, dess.");
+                break;
+            case 7 : message.channel.send("Tu m’as pourtant dit qu’on allait jouer ensemble, dess.");
+                break;
+            case 8 : message.channel.send("**ronronnement**");
+                break;
+            case 9 : message.channel.send("Tu es très con, mais tu sais bien caresser, je t’autorise à respirer le même air que moi, dess.");
+                break;
+            case 10 : message.channel.send("Je suis une fennec, pas un chat. Et le premier qui me prend pour son chien, je le bute, dess.");
+                break;
+            case 11 : message.channel.send("C’est quand qu’on joue, dess ?");
+                break ;
+            case 12 : message.channel.send("Pourquoi je dis dess, dess ? Parce que, espèce de mouche à merde, dess.");
+                break ;
+            case 13 : message.channel.send("Mmm, les cookies c’est kroo bonn, donne m’en encore esclave, dess. ");
+                break ;
+            case 14 : message.channel.send("Dess **lève les bras toute contente.**");
+                break ;
+            case 15 : message.channel.send("Tu cuisines bien, je t’aime bien, dess.");
+                break ;                            
+            case 16 : message.channel.send("Je peux manger ce poisson là bas, dess ? Quoi ? Une sirène, dess ? **Grimace de dégoût.**");
+                break ;
+            case 17 : message.channel.send("Regarde, regarde, dess. Un pigeon, dess. **Dit elle en pointant du doigt une Flügel.**");
+                break ;
+            case 18 : message.channel.send("Les dhampires aiment bien sucer, dess, du sang, dess.");
+                break ;
+            case 19 : message.channel.send("Comment font ces vegans d’elfes pour avoir des gros lolos, dess ? ");
+                break ;
+            case 20 : message.channel.send("Quel est le chemin le plus court pour atteindre le cœur, dess ? Quelle question idiote, dess. Écoute moi bien ex-machina, dess. Le meilleur moyen d’atteindre le cœur d’une personne c’est en broyant sa cage thoracique, dess.");
+                break ;
+            case 21 : message.channel.send("Je suis ambassadrice et mineure, si tu penses pouvoir m’impressionner tu peux te le mettre dans le cul, dess.");
+                break ;
+            case 22 : message.channel.send("(Izuna version internationale) You are bitch, please.");
+                break ;
+            case 23 : message.channel.send("**Mange un cookie.**");
+                break ;
+            case 24 : message.channel.send("**Dort avec dans ses bras une pancarte disant :** « Le premier qui me réveille, je lui arrache sa rate pour le bien de tous. »");
+                break ;
+            case 25 : message.channel.send("BAKA BAKA BAKA, dess !");
+                break ;
+        }
     }
 });
 
